e0ede1f28aa8b07882a02858e320857aab0c08b5
Souptik-Coder <69388347+Souptik-Coder@users.noreply.github.com>
2024-08-23 14:27:01 +0530

    Week 4

---
+++ Week 4,5/BookStoreAPI/src/test/java/com/example/BookStoreAPI/BookStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookStoreAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
